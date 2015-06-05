6e2491a6a9ad4dfdddbe90c832f74d069bad5ea1
Jong23 <joernhuebner@freenet.de>
2015-06-05 16:09:35 +0200

    added conditions basic expandable listview

--- app/src/main/java/org/dhbw/geo/RuleAction.java
+++ app/src/main/java/org/dhbw/geo/RuleAction.java
@@ -3,17 +3,55 @@ package org.dhbw.geo;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
+
+import org.dhbw.geo.ListView.*;
+
+import java.util.ArrayList;
 
 
 public class RuleAction extends Activity {
+    // more efficient than HashMap for mapping integers to objects
+    ArrayList<Group> groups = new ArrayList<Group>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rule_action);
+        createData();
+        ExpandableListView listView = (ExpandableListView) findViewById(R.id.expandableListView);
+        MyExpandableListAdapter adapter = new MyExpandableListAdapter(this,
+                groups);
+        listView.setAdapter(adapter);
+    }
+
+    public void createData() {
+        /*for (int j = 0; j < 5; j++) {
+            Group group = new Group("Test " + j);
+            for (int i = 0; i < 5; i++) {
+                group.children.add(new Child("child" + i, false));
+            }
+            groups.append(j, group);
+        }*/
+
+        Group group = new Group ("Wlan");
+        group.add(new Child("Active", true));
+        group.add(new Child("Status",true));
+
+        Group group2 = new Group("Sound");
+        group2.add(new Child("Active",true));
+
+        groups.add(group);
+        groups.add(group2);
+
+
     }
 
 
 }
+
+
+
