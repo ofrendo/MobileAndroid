f49fd95f454a9b5a644d66b378ed3900f8bb71bd
Max <maggshofmann@aol.com>
2015-06-22 15:43:42 +0200

    map/fix back button

--- app/src/main/java/org/dhbw/geo/Map/Maps.java
+++ app/src/main/java/org/dhbw/geo/Map/Maps.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
@@ -72,6 +73,25 @@ public class Maps extends ActionBarActivity implements GoogleMap.OnMarkerClickLi
     DBConditionFence fenceGroup;
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+        @Override
     public void onBackPressed() {
         Intent parent = getParentActivityIntent();
         //pls enter ruleID
